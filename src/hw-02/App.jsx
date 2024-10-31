ecac72d467226ff46cfcaa92be05a45ddcdb5c8b
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-10-31 01:28:46 +0100

    Homework 2

--- src/hw-02/App.jsx
+++ src/hw-02/App.jsx
@@ -1,3 +1,93 @@
+import { useEffect, useState } from "react";
+import css from "./App.module.css";
+
+const If = ({ children, condition }) => (condition ? children : null);
+
+const Description = () => (
+  <header>
+    <h1>Sip Happens Café</h1>
+    <p>
+      Please leave your feedback about our service by selecting one of the
+      options below.
+    </p>
+  </header>
+);
+
+const Options = ({ updateFeedback, totalFeedback }) => (
+  <section>
+    <button onClick={() => updateFeedback("good")}>Good</button>
+    <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+    <button onClick={() => updateFeedback("bad")}>Bad</button>
+
+    <If condition={totalFeedback >= 1}>
+      <button onClick={() => updateFeedback("reset")}>Reset</button>
+    </If>
+  </section>
+);
+
+const Feedback = ({ feedbacks: { good, neutral, bad }, total, positive }) => (
+  <section>
+    <p>Good: {good}</p>
+    <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <p>Total: {total}</p>
+    <p>Positive: {positive}%</p>
+  </section>
+);
+
+const Notification = () => <p>No feedback yet</p>;
+
+const initialFeedbacks = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+const LOCAL_STORAGE_KEY = "cafe_feedback";
+
 export const Homework_02 = () => {
-  return <main>App</main>;
+  const [feedbacks, setFeedbacks] = useState(() => {
+    const savedFeedback = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+    return savedFeedback === null
+      ? initialFeedbacks
+      : JSON.parse(savedFeedback);
+  });
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(feedbacks));
+  }, [feedbacks]);
+
+  const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
+  const showFeedback = totalFeedback >= 1;
+
+  const positiveFeedback = Math.round(
+    ((feedbacks.good + feedbacks.neutral) / totalFeedback) * 100
+  );
+
+  const updateFeedback = (feedbackType) => {
+    if (feedbackType === "reset") return setFeedbacks(initialFeedbacks);
+
+    setFeedbacks((prev) => ({
+      ...prev,
+      [feedbackType]: prev[feedbackType] + 1,
+    }));
+  };
+
+  return (
+    <main className={css.cafe}>
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+
+      {showFeedback ? (
+        <Feedback
+          feedbacks={feedbacks}
+          total={totalFeedback}
+          positive={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </main>
+  );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Homework_01 } from "./hw-01/App.jsx";
-// import { Homework_02 } from "./hw-02/App.jsx";
+import { Homework_02 } from "./hw-02/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Homework_01 />
-    {/* <Homework_02 /> */}
+    {/* <Homework_01 /> */}
+    <Homework_02 />
   </StrictMode>
 );
